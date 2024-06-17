af9343c185fd88628b34f785ceab6b3b0960c3dd
siddarth2810 <siddarthg0910@gmail.com>
2024-06-17 16:45:26 +0530

    add auth middleware

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import api from '../api';
+import { useEffect } from 'react';
 const HomePage = () => {
 
+    const getUserData = async () => {
+
+        try {
+            const res = await api.post('api/v1/user/getUserData', {},
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('token')}`
+                    },
+                })
+
+        } catch (err) {
+            console.log(err)
+        }
+
+    }
+    useEffect(() => {
+        getUserData();
+    }, []);
     return (
         <>
             <h2>homepage</h2>
